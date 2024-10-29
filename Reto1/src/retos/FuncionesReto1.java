a813d6fb5e8ef4d702cefeb7d8291f5dd1f786ee
GRDinculescu <grdinculescu@gmail.com>
2024-10-29 10:54:15 +0100

    Funciones de Rafa creadas.

--- Reto1/src/retos/FuncionesReto1.java
+++ Reto1/src/retos/FuncionesReto1.java
@@ -1,8 +1,6 @@
 package retos;
 
 public class FuncionesReto1 {
-	
-	
 	// Operaciones
 	public static int sumar(int a, int b) {
 		return a+b;
@@ -13,10 +11,31 @@ public class FuncionesReto1 {
 	
 	// Comprobaciones
 	public static boolean esPar(int a) {
-		return (a%2==0)?true:false;
+		return a % 2 == 0;
 	}
 	public static boolean esImpar(int a) {
-		return (a%2==1)?true:false;
+		return a % 2 == 1;
 	}
 
+
+	// Operaciones matematicas
+	public static int multiplicar(int a, int b) {
+		return a*b;
+	}
+	public static float dividir(int a, int b) {
+		if(b!=0) {
+			return (float)a/b;
+		} else {
+			System.out.println("No se puede dividir entre 0");
+			return 0.f;
+		}
+	}
+
+	// Comprobar
+	public static boolean esPositivo(int a) {
+		return a >= 0;
+	}
+	public static boolean esNegativo(int a) {
+		return a < 0;
+	}
 }
--- Reto1/src/retos/Reto1.java
+++ Reto1/src/retos/Reto1.java
@@ -3,9 +3,5 @@ package retos;
 public class Reto1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(Funciones.sumar(1,5));
-		
 	}
-
 }
