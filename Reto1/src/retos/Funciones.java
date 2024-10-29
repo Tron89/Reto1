7a70a8cbeb5829c2e656eec2163c73d9cee96ec6
GRDinculescu <grdinculescu@gmail.com>
2024-10-29 10:48:12 +0100

    Funciones de Rafa creadas.

--- Reto1/src/retos/Funciones.java
+++ Reto1/src/retos/Funciones.java
@@ -7,4 +7,57 @@ public class Funciones {
 	}
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	public static boolean esPar(int a) {
+		return a % 2 == 0;
+	}
+	public static boolean esImpar(int a) {
+		return a % 2 == 1;
+	}
+	public static boolean esPositivo(int a) {
+		return a >= 0;
+	}
+	public static boolean esNegativo(int a) {
+		return a < 0;
+	}
 }
