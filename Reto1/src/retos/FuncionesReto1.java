5bfdf44c6a4d674fccfafc783635079da4d409fb
Tron89 <claudiuacricota@gmail.com>
2024-10-29 10:43:43 +0100

    Operaciones basicas y comprobaciones basicas

---
+++ Reto1/src/retos/FuncionesReto1.java
@@ -0,0 +1,22 @@
+package retos;
+
+public class FuncionesReto1 {
+	
+	
+	// Operaciones
+	public static int sumar(int a, int b) {
+		return a+b;
+	}
+	public static int restar(int a, int b) {
+		return a-b;
+	}
+	
+	// Comprobaciones
+	public static boolean esPar(int a) {
+		return (a%2==0)?true:false;
+	}
+	public static boolean esImpar(int a) {
+		return (a%2==1)?true:false;
+	}
+
+}
--- Reto1/src/retos/Reto1.java
+++ Reto1/src/retos/Reto1.java
@@ -4,7 +4,7 @@ public class Reto1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(Funciones.sumar(1,5));
+		System.out.println(FuncionesReto1.sumar(1,5));
 		
 	}
 
